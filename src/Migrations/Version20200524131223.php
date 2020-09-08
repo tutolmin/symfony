1308a550db6a0a300c16fee574289d2c6fae413b
Andrey Tutolmin <tutolmin@gmail.com>
2020-09-08 15:53:15 -0400

    Migrations fix

--- src/Migrations/Version20200524131223.php
+++ src/Migrations/Version20200524131223.php
@@ -22,7 +22,6 @@ final class Version20200524131223 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE user_upload_game_task');
         $this->addSql('ALTER TABLE user ADD first_name VARCHAR(255) DEFAULT NULL, ADD last_name VARCHAR(255) DEFAULT NULL');
     }
 
@@ -31,8 +30,6 @@ final class Version20200524131223 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE user_upload_game_task (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, hash VARCHAR(255) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_F9E117FBA76ED395 (user_id), UNIQUE INDEX UNIQ_F9E117FBD1B862B8 (hash), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE user_upload_game_task ADD CONSTRAINT FK_F9E117FBA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE user DROP first_name, DROP last_name');
     }
 }
