691ddfec672b294b07bb7b2080bf3ed1087659ca
Andrey Tutolmin <tutolmin@gmail.com>
2020-10-08 11:38:22 +0300

    cache-control for static content

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,16 +2,18 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\EventListener\AbstractSessionListener;
 
 class LuckyController extends AbstractController
 {
      /**
       * @Route("/lucky/number")
       */
-    public function number(): Response
+    public function number(Request $request): Response
     {
         $number = random_int(0, 100);
 /*
@@ -19,9 +21,21 @@ class LuckyController extends AbstractController
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
 */
-        return $this->render('lucky/number.html.twig', [
+
+        $response = $this->render('lucky/number.html.twig', [
             'number' => $number,
         ]);
+
+        $response->headers->set(AbstractSessionListener::NO_AUTO_CACHE_CONTROL_HEADER, 'true');
+
+        $response->setETag(md5($response->getContent()));
+        $response->setPublic(); // make sure the response is public/cacheable
+        $response->isNotModified($request);
+        // set the private or shared max age
+        $response->setMaxAge(600);
+        $response->setSharedMaxAge(600);
+
+        return $response;
     }
 }
 ?>
--- src/Controller/StaticContentController.php
+++ src/Controller/StaticContentController.php
@@ -4,39 +4,81 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Symfony\Component\HttpKernel\EventListener\AbstractSessionListener;
 
 class StaticContentController extends AbstractController
 {
     /**
      * @Route("/terms", name="terms")
+     * @Cache(expires="+1 week")
      */
-    public function tos(): Response
+    public function tos(Request $request): Response
     {
-        return $this->render('terms.html.twig', [
+        // Generate content with template
+        $response = $this->render('terms.html.twig', [
             'controller_name' => 'StaticContentController',
-            'gid' =>  0,
         ]);
+
+        // Strip automatic Cache-Control header for the user session
+        $response->headers->set(AbstractSessionListener::NO_AUTO_CACHE_CONTROL_HEADER, 'true');
+
+        // Generate ETag
+        $response->setETag(md5($response->getContent()));
+        $response->setPublic(); // make sure the response is public/cacheable
+        $response->isNotModified($request);
+        $response->setMaxAge(604800);
+        $response->setSharedMaxAge(604800);
+
+        return $response;
     }
     /**
      * @Route("/privacy", name="privacy")
+     * @Cache(expires="+1 week")
      */
     public function privacy(): Response
     {
-        return $this->render('privacy.html.twig', [
-            'controller_name' => 'StaticContentController',
-            'gid' =>  0,
-        ]);
+      // Generate content with template
+      $response = $this->render('privacy.html.twig', [
+          'controller_name' => 'StaticContentController',
+      ]);
+
+      // Strip automatic Cache-Control header for the user session
+      $response->headers->set(AbstractSessionListener::NO_AUTO_CACHE_CONTROL_HEADER, 'true');
+
+      // Generate ETag
+      $response->setETag(md5($response->getContent()));
+      $response->setPublic(); // make sure the response is public/cacheable
+      $response->isNotModified($request);
+      $response->setMaxAge(604800);
+      $response->setSharedMaxAge(604800);
+
+      return $response;
     }
     /**
      * @Route("/about", name="about")
+     * @Cache(expires="+1 week")
      */
     public function about(): Response
     {
-        return $this->render('about.html.twig', [
-            'controller_name' => 'StaticContentController',
-            'gid' =>  0,
-        ]);
+      // Generate content with template
+      $response = $this->render('about.html.twig', [
+          'controller_name' => 'StaticContentController',
+      ]);
+
+      // Strip automatic Cache-Control header for the user session
+      $response->headers->set(AbstractSessionListener::NO_AUTO_CACHE_CONTROL_HEADER, 'true');
+
+      // Generate ETag
+      $response->setETag(md5($response->getContent()));
+      $response->setPublic(); // make sure the response is public/cacheable
+      $response->isNotModified($request);
+      $response->setMaxAge(604800);
+      $response->setSharedMaxAge(604800);
+
+      return $response;
     }
 }
 ?>
